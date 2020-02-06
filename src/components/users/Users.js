7541ffe7e3fa3b62cc36f80a5cc8449d71ac4889
Conor Walsh <conorwalsh0703@gmail.com>
2020-02-06 23:19:10 +0000

    added navbar and users components

---
+++ src/components/users/Users.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import UserItem from "./UserItem";
+
+class Users extends Component {
+  state = {
+    users: [
+      {
+        id: "1",
+        login: "walshyc",
+        avatar_url: "https://avatars0.githubusercontent.com/u/18621742?v=4",
+        html_url: "https://github.com/walshyc"
+      },
+      {
+        id: "2",
+        login: "mojombo2",
+        avatar_url: "https://avatars0.githubusercontent.com/u/2?v=4",
+        html_url: "https://github.com/defunkt"
+      },
+      {
+        id: "3",
+        login: "mojombo3",
+        avatar_url: "https://avatars0.githubusercontent.com/u/3?v=4",
+        html_url: "https://github.com/pjhyett"
+      }
+    ]
+  };
+  render() {
+    return (
+      <div style={userStyle}>
+        {this.state.users.map(user => (
+          <UserItem key={user.id} user={user}></UserItem>
+        ))}
+      </div>
+    );
+  }
+}
+
+const userStyle = {
+  display: 'grid',
+  gridTemplateColumns: 'repeat(3, 1fr)',
+  gridGap : '1rem'
+}
+
+export default Users;
