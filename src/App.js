86287273973bf431e74febf889997d4cd401b834
Conor Walsh <conorwalsh0703@gmail.com>
2020-02-10 16:13:52 +0000

    added loading spinner

--- src/App.js
+++ src/App.js
@@ -2,14 +2,25 @@ import React, { Component } from "react";
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
 import "./App.css";
+import axios from "axios";
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  };
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const res = await axios.get("https://api.github.com/users");
+    this.setState({ users: res.data, loading: false });
+  }
+
   render() {
     return (
       <div className="App">
         <Navbar> </Navbar>
         <div className="container">
-          <Users></Users>
+          <Users users={this.state.users} loading={this.state.loading}></Users>
         </div>
       </div>
     );
